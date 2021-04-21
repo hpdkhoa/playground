8dc477cb820a21d7a03db839f3b1ae82d0ac6441
hpdkhoa <hpdkhoa2311@gmail.com>
2021-04-21 21:16:53 +0700

    Initialize the demo for calling java in HTML with some problems inside

---
+++ htmlDemo/src/test/java/com/example/htmlDemo/HtmlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.htmlDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
